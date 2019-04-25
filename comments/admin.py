3ee2bad07a01aa5fc31a6c47c7289b3089f02009
emhui <251508638@qq.com>
2019-04-25 08:41:27 +0800

    美化评论管理界面

--- comments/admin.py
+++ comments/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from .models import Comment
 # Register your models here.
 
-admin.site.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name','email','url']
+admin.site.register(Comment, CommentAdmin)
